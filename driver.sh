90d667008f27e40ff4345aade12ee972d7531ba8
KAI10 <ikaziashik@gmail.com>
2025-08-04 13:35:29 -0400

    fixed driver.sh

--- driver.sh
+++ driver.sh
@@ -18,7 +18,11 @@ do
   echo "Submitting Job with: patchSize $patchSize numPatches $numPatches ECH $ECH, COV $COV, MASKP $MASKP Seed $i ..."
 
   mkdir -p "$storePathPrefix"/$patchSize/num_patches_"$numPatches"_ECH_"$ECH"_COV_"$COV"/seed_"$i"
-  sbatch --esrker.sh > "$storePathPrefix"/"$patchSize"/num_patches_"$numPatches"_ECH_"$ECH"_COV_"$COV"/seed_"$i"/train.jobid
+  sbatch --export=seed="$i",storePath="$storePathPrefix"/$patchSize/num_patches_"$numPatches"_ECH_"$ECH"_COV_"$COV"/seed_"$i"/ \
+          --job-name=DF-"$patchSize"-"$numPatches"_ECH_"$ECH"_COV_"$COV"_MASKP_"$MASKP"-"$i" \
+          --error="$storePathPrefix"/"$patchSize"/num_patches_"$numPatches"_ECH_"$ECH"_COV_"$COV"/seed_"$i"/train.err \
+          --output="$storePathPrefix"/"$patchSize"/num_patches_"$numPatches"_ECH_"$ECH"_COV_"$COV"/seed_"$i"/train.out \
+          worker.sh > "$storePathPrefix"/"$patchSize"/num_patches_"$numPatches"_ECH_"$ECH"_COV_"$COV"/seed_"$i"/train.jobid
 
   # sbatch --export=seed="$i",storePath="$storePathPrefix"/$patchSize/num_patches_"$numPatches"_ECH_"$ECH"_COV_"$COV"/seed_"$i"/ \
   #         --job-name=DF-Test-"$patchSize"-"$numPatches"_ECH_"$ECH"_COV_"$COV"_MASKP_"$MASKP"_HORIZON_"$horizonLength"-"$i" \
